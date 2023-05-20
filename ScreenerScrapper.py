e6d25a3c870a26d5fc77c4ca53d011fc1ca3ca5a
Prasanth Varghese <mail2prasanthv@gmail.com>
2023-05-20 20:46:10 +0530

    Changes-1

--- ScreenerScrapper.py
+++ ScreenerScrapper.py
@@ -62,57 +62,57 @@ def getSectorAndIndustry(soup):
     basic_dict['industries'] = industries
     return basic_dict
 
-def getCashFlows(soup):
-    net_cash_flow_dict= {}
-    operating_activity_dict= {}
-    investing_activity_dict={}
-    financing_activity_dict={}
-    section_children = soup.find("section", {"id": "cash-flow"})
-    years = getHeaderInYears(section_children)
-    #
-    cashflow_activities = section_children.find_all("tr")
-    cashflow_operating_activity= cashflow_activities[1]
-    cashflow_investing_activity= cashflow_activities[2]
-    cashflow_financing_activity= cashflow_activities[3]
+# def getCashFlows(soup):
+#     net_cash_flow_dict= {}
+#     operating_activity_dict= {}
+#     investing_activity_dict={}
+#     financing_activity_dict={}
+#     section_children = soup.find("section", {"id": "cash-flow"})
+#     years = getHeaderInYears(section_children)
+#     #
+#     cashflow_activities = section_children.find_all("tr")
+#     cashflow_operating_activity= cashflow_activities[1]
+#     cashflow_investing_activity= cashflow_activities[2]
+#     cashflow_financing_activity= cashflow_activities[3]
 
-    operating_activity_dict = getTimePerioddata(years, cashflow_operating_activity)
-    investing_activity_dict = getTimePerioddata(years, cashflow_investing_activity)
-    financing_activity_dict = getTimePerioddata(years, cashflow_financing_activity)
+#     operating_activity_dict = getTimePerioddata(years, cashflow_operating_activity)
+#     investing_activity_dict = getTimePerioddata(years, cashflow_investing_activity)
+#     financing_activity_dict = getTimePerioddata(years, cashflow_financing_activity)
 
    
-    index =0
-    #Total
-    net_cash_flows = section_children.find("tr", {"class": "strong"})
-    net_cash_flow_dict = getTimePerioddata(years, net_cash_flows)
+#     index =0
+#     #Total
+#     net_cash_flows = section_children.find("tr", {"class": "strong"})
+#     net_cash_flow_dict = getTimePerioddata(years, net_cash_flows)
 
-    return operating_activity_dict, investing_activity_dict, financing_activity_dict, net_cash_flow_dict
+#     return operating_activity_dict, investing_activity_dict, financing_activity_dict, net_cash_flow_dict
 
 
-def getBalanceSheet(soup):
-    balancesheet_historical_assets = {}
-    balancesheet_historical_liability = {}
-    section_children = soup.find("section", {"id": "balance-sheet"})
-    years = getHeaderInYears(section_children)
+# def getBalanceSheet(soup):
+#     balancesheet_historical_assets = {}
+#     balancesheet_historical_liability = {}
+#     section_children = soup.find("section", {"id": "balance-sheet"})
+#     years = getHeaderInYears(section_children)
         
-    assets_liabilities = section_children.find_all("tr", {"class": "strong"})
-    liabilities_all = assets_liabilities[0]
-    assets_all = assets_liabilities[1]
-    liabilities = liabilities_all.find_all("td", {"class": ""})
-    assets = assets_all.find_all("td", {"class": ""})
+#     assets_liabilities = section_children.find_all("tr", {"class": "strong"})
+#     liabilities_all = assets_liabilities[0]
+#     assets_all = assets_liabilities[1]
+#     liabilities = liabilities_all.find_all("td", {"class": ""})
+#     assets = assets_all.find_all("td", {"class": ""})
 
-    index =0
-    for liability in liabilities:
-        liability_text = removeunwantedCharsInNumber(liability.text)
-        balancesheet_historical_liability[years[index]] = liability_text
-        index = index+1
+#     index =0
+#     for liability in liabilities:
+#         liability_text = removeunwantedCharsInNumber(liability.text)
+#         balancesheet_historical_liability[years[index]] = liability_text
+#         index = index+1
 
-    index =0
-    for asset in assets:
-        asset_text = removeunwantedCharsInNumber(asset.text)
-        balancesheet_historical_assets[years[index]] = asset_text
-        index = index+1
+#     index =0
+#     for asset in assets:
+#         asset_text = removeunwantedCharsInNumber(asset.text)
+#         balancesheet_historical_assets[years[index]] = asset_text
+#         index = index+1
 
-    return balancesheet_historical_assets, balancesheet_historical_liability
+#     return balancesheet_historical_assets, balancesheet_historical_liability
 
 def getHeaderInQuarters(section_children):
     quarters = []
@@ -149,17 +149,37 @@ def getClassDataText(soup, classname):
 def getClassData(soup, classname):
     return soup.find(class_=classname)
 
+def getShareHoldingPattern(soup):
+    section_children = soup.find("section", {"id": "shareholding"})
+    return getGeneralData(section_children,"Quarterly")
+
 def getQuarterlyResults(soup):
     section_children = soup.find("section", {"id": "quarters"})
-    return getGeneralData(section_children)
+    return getGeneralData(section_children,"Quarterly")
+
+def getRatios(soup):
+    section_children = soup.find("section", {"id": "ratios"})
+    return getGeneralData(section_children,"Yearly")
 
 def getProfitAndLoss(soup):
     section_children = soup.find("section", {"id": "profit-loss"})
-    return getGeneralData(section_children)
+    return getGeneralData(section_children,"Yearly")
 
-def getGeneralData(section_children):
-    quarters = getHeaderInQuarters(section_children)
-    
+def getBalanceSheetNew(soup):
+    section_children = soup.find("section", {"id": "balance-sheet"})
+    return getGeneralData(section_children,"Yearly")
+
+def getCashFlowsNew(soup):
+    section_children = soup.find("section", {"id": "cash-flow"})
+    return getGeneralData(section_children, "Yearly")
+
+def getGeneralData(section_children, period_frequency):
+    period=None
+    if(period_frequency=='Quarterly'):
+        period = getHeaderInQuarters(section_children)
+    elif(period_frequency=='Yearly'):
+        period = getHeaderInYears(section_children)
+
     table_body = section_children.find("tbody")
     rows= table_body.find_all("tr")
     superdict ={}
@@ -180,7 +200,7 @@ def getGeneralData(section_children):
                 continue
             if(key=="RawPDF"):
                 continue
-            dict[quarters[index]] = value
+            dict[period[index-1]] = value
             index = index+1
         if(key!="RawPDF"):
             superdict[key] = dict
@@ -203,11 +223,21 @@ basic_dict1 = getWebsiteBseNseCodes(soup)
 basic_dict2 = getbasicInfo(soup)
 
 basic_dict3 =  getSectorAndIndustry(soup)
-balancesheet_historical_assets, balancesheet_historical_liability =  getBalanceSheet(soup)
 
-operating_activity_dict, investing_activity_dict, financing_activity_dict, net_cash_flow_dict = getCashFlows(soup)
+print("-------------QuarterlyResults-------------")
+print(getQuarterlyResults(soup))
 
-# print(operating_activity_dict, investing_activity_dict, financing_activity_dict, net_cash_flow_dict)
-
-getQuarterlyResults(soup)
+print("-------------ProfitAndLoss-------------")
 print(getProfitAndLoss(soup))
+
+print("-------------BalanceSheetNew-------------")
+print(getBalanceSheetNew(soup))
+
+print("-------------CashFlows-------------")
+print(getCashFlowsNew(soup))
+
+print("-------------Ratios-------------")
+print(getRatios(soup))
+
+print("-------------ShareHoldingPattern-------------")
+print(getShareHoldingPattern(soup))
